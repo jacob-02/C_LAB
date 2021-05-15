5ae14d3f4cd5f6d814eccbab807793f233412346
jackjack3006 <jvsanoj23@gmail.com>
2021-05-15 20:55:26 +0530

    Assignment 2 3rd Question

--- Code/Assignment2_Question3.c
+++ Code/Assignment2_Question3.c
@@ -18,4 +18,55 @@ Enter the bit position, which bit you want to clear
 3
 set : 0
 The number after clear is 17
+
+
+// C Program to demonstrate use of bitwise operators
+
+#include <stdio.h>
+
+int main()
+{
+	int n, k, num;
+	printf("Enter the number\n");
+	scanf("%d", &num);
+	printf("Enter the bit position which you want to clear\n");
+	scanf("%d", &k);
+	printf("set : ");
+	scanf("%d", &n);
+	
+	num = (num & (~(1 << (k - n))));
+	printf("The number after clear is %d\n", num);
+}
+
+
+int  main()
+{
+	int n, k, num;
+	printf("Enter the number\n");
+	scanf("%d", &num);
+	printf("Enter the bit position which you want to set\n");
+	scanf("%d", &k);
+	printf("set : ");
+	scanf("%d", &n);
+	
+	num = (num | (1 << (k - n)));
+	printf("The number after set is %d\n", num);
+}
+
+
+int main()
+{
+	int num, n;
+	printf("Enter the number that you want to check\n");
+	scanf("%d", &num);
+	printf("Input number is %d\n", num);
+	printf("Enter the bit position, starts from zero\n");
+	scanf("%d", &n);
+	
+	if (num & (1<<n))
+		printf("Bit is set\n");
+	
+	else
+		printf("Bit is not set\n");
+}
 */
