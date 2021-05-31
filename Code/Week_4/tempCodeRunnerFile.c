6ab51f50db0007f30f49d4a91c4bea944c912bf0
jackjack3006 <jvsanoj23@gmail.com>
2021-05-31 19:47:56 +0530

    Question 4. Fun :)

--- Code/Week_4/tempCodeRunnerFile.c
+++ Code/Week_4/tempCodeRunnerFile.c
@@ -1,22 +1,2 @@
-#include <stdio.h>
-#include "Question1_reverseindex.c"
-#include "Question1_reversepointer.c"
-
-void main()
-{
-    printf("Enter the size of the array :\n");
-    int n;
-    scanf("%d", &n);
-
-    int array[n];
-
-    printf("Enter the elements of the array :\n");
-
-    for(int i = 0; i < n; i++)
-        scanf("%d", &array[i]);
-    
-    reverse_index(n, array);
-    printf("\n");
-
-    reversearray(array, n);
-}
+else if(count == 0)
+    //     printf("The number is a prime\n");
