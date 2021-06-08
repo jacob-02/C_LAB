fc28a611b801533051293fedb4da150509122336
jackjack3006 <jvsanoj23@gmail.com>
2021-06-08 20:36:07 +0530

    Question 3. Removing repetition

--- Code/Week_5/Question3.c
+++ Code/Week_5/Question3.c
@@ -1,34 +1,12 @@
 #include <stdio.h>
-int length(char arr[])
-{
-    int k = 0;
-    for (int i = 0; arr[i] != '\0'; i++)
-    {
-        k = k + 1;
-    }
-    return k;
-}
+#include "Question3_duplicate.c"
 int main()
 {
-    char a[20], b[20];
-
+    char str[20];
     printf("Enter the string\n");
-    scanf("%s", a);
-    for (int i = 0; a[i] != '\0'; i++)
-    {
-        int check = 0;
-        for (int j = 0; j < i; j++)
-        {
-            if (a[i] == a[j])
-            {
-                check = 1;
-            }
-        }
-        if (check == 0)
-        {
-            b[length(b)] = a[i];
-        }
-    }
-    printf("%s\n", b);
+    scanf("%[^\n]s", str);
+    printf("string before removing duplicates is %s\n", str);
+    removeduplicates(str);
+    printf("string after removing duplicates is %s\n", str);
     return 0;
 }
---
+++ Code/Week_5/Question3_duplicate.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void removeall(char *str, char remove, int index)
+{
+    int i;
+    while (str[index] != '\0')
+    {
+        if (str[index] == remove)
+        {
+            i = index;
+            while (str[i] != '\0')
+            {
+                str[i] = str[i + 1];
+                i++;
+            }
+        }
+        else
+            index++;
+    }
+}
+
+void removeduplicates(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        removeall(str, str[i], i + 1);
+        i++;
+    }
+}
