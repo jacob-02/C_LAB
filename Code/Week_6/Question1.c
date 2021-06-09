7410e039ca5548e780596d9dbcef63f9d4c91080
jackjack3006 <jvsanoj23@gmail.com>
2021-06-09 22:55:16 +0530

    Figuring out the i and j values

--- Code/Week_6/Question1.c
+++ Code/Week_6/Question1.c
@@ -2,15 +2,27 @@
 
 int main()
 {
-    int array[20][20];
+    int array[5][5];
 
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 5; i++)
     {
-        for (int j = 0; j < 20; j++)
+        for (int j = 0; j < i; j++)
         {
-            array[i][j] = 
-        }
-        
+            if(i == j + 1 || j == 0)
+                array[i][j] = 1;
+            else
+                array[i][j] = (i + j + 1);
+        }   
     }
-    
+
+    // printf("%d\n", array[10][10]);
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("%d ", array[i][j]);
+        }  
+        printf("\n");
+    }   
 }
