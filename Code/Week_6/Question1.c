db8812896a74f23a8166ff9db7afb17895370f14
jackjack3006 <jvsanoj23@gmail.com>
2021-06-09 22:42:49 +0530

    Week 5 initial commit

---
+++ Code/Week_6/Question1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int array[20][20];
+
+    for (int i = 0; i < 20; i++)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            array[i][j] = 
+        }
+        
+    }
+    
+}
